2ca9970a2ee2e33ff7cefad29ee85afb9d7f6325
Saul <saulnike111@gmail.com>
2024-06-12 19:31:33 -0500

    modelo prodcutor

---
+++ src/models/gestionProductor.js
@@ -0,0 +1,33 @@
+const productor ={
+
+    // para crear producto
+    crearProductoModelo(){
+        // código
+    }
+    ,
+
+    // para actualizar producto 
+    actualizarProductoModelo(){
+        // código
+    }
+    ,
+    // para eliminar producto
+    eliminarProductoModelo(){
+        // código
+    }
+    ,
+
+    // para ver  todos los productos productos
+    verTodosProductosModelo(){
+        // código
+    }
+    ,
+
+    // para ver producto especifico 
+    verProductoModelo(){
+        // código
+    }
+    
+}
+    
+    export default productor
